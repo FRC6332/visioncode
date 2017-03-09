145cea0c6c8ebdd07ee8150bb55410da64a9988b
Will Shamblin <will@shamblin.org>
2017-03-09 08:46:53 -0500

    Added PWM code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # visioncode
+# visioncode
--- tape.py
+++ tape.py
@@ -5,6 +5,14 @@ import numpy as np
 import json
 #import imutils
 import cv2
+# Import PWM library
+import wiringpi2
+OUTPUT=1
+PIN_TO_PWM = 30
+wiringpi2.wiringPiSetup()
+wiringpi2.pinMode(PIN_TO_PWM,OUTPUT)
+wiringpi2.digitalWrite(PIN_TO_PWM,1)
+wiringpi2.softPwmCreate(PIN_TO_PWM,0,1023)
 min=10000000000
 # construct the argument parse and parse the arguments
 #ap = argparse.ArgumentParser()
@@ -18,8 +26,8 @@ min=10000000000
 # list of tracked points
 #greenLower = (29, 86, 6)
 #greenUpper = (64, 255, 255)
-greenLower = np.array([75,200,200])
-greenUpper = np.array([255,255,255])
+greenLower = np.array([75,100,100])
+greenUpper = np.array([140,255,255])
 #pts = deque(maxlen=args["buffer"])
 # if a video path was not supplied, grab the reference
 # to the webcam
@@ -28,6 +36,8 @@ camera = cv2.VideoCapture(0)
 camera.set(3,640)
 camera.set(3,480)
 minlist=[]
+print camera.get(cv2.CAP_PROP_FRAME_WIDTH)
+print camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
 #cv2.VideoCapture.set(CV_CAP_PROP_FPS, 30)
 # otherwise, grab a reference to the video file
 #else:
@@ -101,26 +111,29 @@ while True:
 					#print min
 				#minlst=
 		minlist = sorted(minlist, key=lambda x: x['rank'])
+#		print minlist
 		#print json.dumps(minlist, indent=2)
 		#if len(minlist) > 0:
 		#	cv2.rectangle(frame, (minlist[0]['xcoord'],minlist[0]['ycoord']), (minlist[0]['xcoord']+minlist[0]['width'],minlist[0]['ycoord']+minlist[0]['height']), (0,255,0), 4)
 		if len(minlist) > 1:
 			if minlist[0]['width']-10 <= minlist[1]['width'] <= minlist[0]['width'] + 10 and minlist[0]['height']-10 <= minlist[1]['height'] <= minlist[0]['height']+10:
-				#if minlist[0]['ycoord']-10 <= minlist[1]['ycoord'] >= minlist[0]['ycoord']+10:
+				if minlist[0]['ycoord']-10 <= minlist[1]['ycoord'] <= minlist[0]['ycoord']+10:
 				#if 1.45 <= float(abs(minlist[0]['xcoord'] - minlist[1]['xcoord']))/float(minlist[0]['width']) <= 1.95:
-				cv2.rectangle(frame, (minlist[0]['xcoord'],minlist[0]['ycoord']), (minlist[0]['xcoord']+minlist[0]['width'],minlist[0]['ycoord']+minlist[0]['height']), (0,255,0), 4)
-				cv2.rectangle(frame, (minlist[1]['xcoord'],minlist[1]['ycoord']), (minlist[1]['xcoord']+minlist[1]['width'],minlist[1]['ycoord']+minlist[1]['height']), (0,255,0), 4)
+					cv2.rectangle(frame, (minlist[0]['xcoord'],minlist[0]['ycoord']), (minlist[0]['xcoord']+minlist[0]['width'],minlist[0]['ycoord']+minlist[0]['height']), (0,255,0), 4)
+					cv2.rectangle(frame, (minlist[1]['xcoord'],minlist[1]['ycoord']), (minlist[1]['xcoord']+minlist[1]['width'],minlist[1]['ycoord']+minlist[1]['height']), (0,255,0), 4)
 				###########################
-				xwx=((float(minlist[0]['xcoord']+minlist[0]['width']) + float(minlist[1]['xcoord']))/2.0)
-				wdth=(float(camera.get(cv2.CAP_PROP_FRAME_WIDTH))/2.0)
-				print (wdth-xwx)*0.10375
-				if xwx <= wdth-5.0:
-					print "Turn Left"
-				elif xwx >= wdth+5.0:
-					print "Turn Right"
-				else:
-					print "Centered"
+					xwx=((float(minlist[0]['xcoord']+minlist[0]['width']) + float(minlist[1]['xcoord']))/2.0)
+					wdth=(float(camera.get(cv2.CAP_PROP_FRAME_WIDTH))/2.0)
+					print (wdth-xwx)*0.10375
+					wiringpi2.softPwmWrite(PIN_TO_PWM,((float(1023)/float(640))*x))
+					#if xwx <= wdth-5.0:
+					#	print "Turn Left"
+					#elif xwx >= wdth+5.0:
+					#	print "Turn Right"
+					#else:
+					#	print "Centered"
 				###########################
+#		print minlist
 		minlist=[]
 			#(x,y,w,h) = ecv2.boundingRect(cnt)
 	   	 	#cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 3)
